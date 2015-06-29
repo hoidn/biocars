a30bf8da0c6490b986a0c1475e2b9f168ef4eb42
IDB user <useridb@helix.cars.aps.anl.gov>
2015-06-29 17:25:17 -0500

    working, but need to change directory management

--- sum_images_parallel.py
+++ sum_images_parallel.py
@@ -1,8 +1,11 @@
 import numpy as np
+import os
 import pdb
 import glob
 from PIL import Image
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from scipy import interpolate
 
 #TODO implement filtering and parallelism
 def sumImages(filename_list, num_cores, filter = None):
@@ -10,14 +13,37 @@ def sumImages(filename_list, num_cores, filter = None):
         stride = len(lst)/num_cores
         sublists = [lst[i * stride: (i + 1) * stride] for i in xrange(num_cores)]
         return sublists
-    if len(filename_list) < 2:
-        return
+    if len(filename_list) < 1:
+        raise ValueError("need at least one file")
     imsum = np.array(Image.open(filename_list[0]))
+    if len(filename_list) == 1:
+        return imsum
     for fname in filename_list[1:]:
         imsum += np.array(Image.open(filename_list[0]))
+    return imsum
 
-#TODO: check that this works
-def radialSum(data, center = None):
+##TODO implement filtering and parallelism
+#def parallel_sum_images(filename_list, num_cores, filter = None):
+#    def partition_list(lst):
+#        stride = len(lst)/num_cores
+#        sublists = [lst[i * stride: (i + 1) * stride] for i in xrange(num_cores)]
+#        return sublists
+#    def single_threaded_sum(lst):
+#        if len(filename_list) < 1:
+#            raise ValueError("need at least one file")
+#        imsum = np.array(Image.open(filename_list[0]))
+#        if len(filename_list) == 1:
+#            return imsum
+#        for fname in filename_list[1:]:
+#            imsum += np.array(Image.open(filename_list[0]))
+#        return imsum
+#    pool = Pool(processes = num_cores)
+#    sub_sums = pool.map(single_threaded_sum, partition_list(filename_list))
+#    return np.sum(
+
+def radialSum(data, distance = 150000., pixSize = 88.6, center = None):
+    if type(data) == str:
+        data = np.array(Image.open(data))
     if center is None:
         l, h = np.shape(data)
         center = [l/2, h/2]
@@ -28,42 +54,101 @@ def radialSum(data, center = None):
     tbin = np.bincount(r.ravel(), data.ravel())
     nr = np.bincount(r.ravel())
     radialprofile = tbin / nr
-    pdb.set_trace()
-    return radialprofile 
+    #pdb.set_trace()
+    rsorted = np.sort(np.unique(r.ravel()))
+    theta = np.arctan(rsorted * pixSize / distance)
+    return np.rad2deg(theta), radialprofile 
+
+
+##normalization modes: peak, if a numerical value is provided it is assumed to be the angle at which 
+##normalization is desired.
+#def plot(name, normalization = "peak"):
+#    def plotOne(name):
+#        r, intensity = np.genfromtxt("runs/" + self.name + "/" + name + "/processed.dat")
+#        if normalization == "peak":
+#            intensity /= np.max(intensity)
+#        elif isinstance(normalization, (int, long, float)):
+#            interpolated = interpolate.interp1d(r, intensity)
+#            normval = interpolated(normalization)
+#            intensity /= normval
+#        plt.plot(r, intensity, label = self.name + "_" + conditionStr)
+#    if condition is None:
+#        plotOne(self.condition)
+#    else:
+#        plotOne(condition)
+
+#def findCenter(data, searchwidth = 1000):
+#    w, h = np.shape(data)
+#    def difference(center):
+#        arrchunk = np.sum(data[w/2 - 100, w/2 + 100, :], axis = 0)
+#        difference 
+
+
+def swap(arr1d, centerindx):
+    breadth = min(centerindx, len(arr1d) - centerindx)
+    newarr = np.zeros(len(arr1d))
+    for i in range(len(breadth)):
+        newarr[centerindx - i], newarr[centerindx + i] = newarr[centerindx + i], newarr[centerindx - i]
+    return np.sum(np.abs(newarr - arr1d))
 
 class Run(object):
-    def __init__(self, name, condition = None):
+    def __init__(self, name, condition = None, center = [1973, 1973]):
+    #def __init__(self, name, condition = None, center = [1984, 1873]):
         if not os.path.isdir("runs/" + name):
-            os.system("mkdir runs/" + name)
+            os.mkdir("runs/" + name)
         self.name = name
         self.switch_condition(condition)
-        self.prefix = "runs/" + name + '/' + self.condition
+        self.center = center #center coords of powder pattern on detector
     
     def switch_condition(self, condition):
         if condition is None:
             self.condition = "default"
-        if not os.path.exists("mkdir runs/" + self.prefix):
-            os.system("mkdir runs/" + self.prefix)
-        self.condition = condition
+        else:
+            self.condition = condition
+	self.prefix = "runs/" + self.name + '/' + self.condition
+        if not os.path.exists(self.prefix):
+            os.mkdir(self.prefix)
 
-    def add_frames(self, files = None):
+    def add_frames(self, files = None, mode = "names"):
+        """
+        mode can equal names or glob
+        """
         if files == None:
-            files = glob.glob("*.tif")
-        newFiles = [prefix + "/" + fname for fname in files]
+            files = glob.glob("*.mccd")
+        if len(files) == 0:
+            print "no new files found"
+            return
+        newFiles = [self.prefix + "/" + fname for fname in files]
         for fname, newfname in zip(files, newFiles):
             os.system("mv " + fname + " " + newfname)
-        summed = sumImages(newFiles)
-        np.tofile(prefix + "/averaged.dat", np.astype(summed, uint32)) 
-        radial = radialSum(summed)
-        np.savetxt(prefix + "/processed.dat")
+        allfiles  = glob.glob(self.prefix + "/*.mccd")
+        summed = sumImages(allfiles, 1)
+        summed.astype("uint32").tofile(self.prefix + "/averaged.dat") 
+        #summed.tofile(self.prefix + "/averaged.dat") 
+        #TODO: is this center value right?
+        radial = radialSum(summed,  center = self.center)
+        np.savetxt(self.prefix + "/processed.dat", radial)
 
-    def plot(condition = None):
+    #normalization modes: peak, if a numerical value is provided it is assumed to be the angle at which 
+    #normalization is desired.
+    def plot(self, condition = None, normalization = "peak"):
         def plotOne(conditionStr):
-            # TODO: implement this format
-            r, intensity = np.genfromtxt("runs/" + self.name + "/" + conditionStr + "processed.dat")
-            plt.plot(r, intensity, conditionStr)
+            r, intensity = np.genfromtxt("runs/" + self.name + "/" + conditionStr + "/processed.dat")
+            if normalization == "peak":
+                intensity /= np.max(intensity)
+            elif isinstance(normalization, (int, long, float)):
+                interpolated = interpolate.interp1d(r, intensity)
+                normval = interpolated(normalization)
+                intensity /= normval
+            plt.plot(r, intensity, label = self.name + "_" + conditionStr)
         if condition is None:
             plotOne(self.condition)
         else:
             plotOne(condition)
+
+    def show(self):
+        plt.xlabel("angle (degrees)")
+        plt.ylabel("inensity (arb)")
+        plt.legend()
+        plt.show()
 
